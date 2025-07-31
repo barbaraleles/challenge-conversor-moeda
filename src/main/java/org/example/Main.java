36ed3fc6c5ecc3d525547d6ce70e7d0a6b400653
barbaraleles <barbara_rlf@hotmail.com>
2025-07-31 12:02:33 -0300

    ajuste do loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -22,30 +22,6 @@ public class Main {
 
         Scanner leia = new Scanner(System.in);
 
-        System.out.println(" *********************************************** ");
-        System.out.println(" Seja bem vindo ao conversor de moedas! ");
-        System.out.print("\n");
-        System.out.println(" Escolha uma opção: ");
-        System.out.println(" 1 - Dólar para Peso Argentino ");
-        System.out.println(" 2 - Peso Argentino para Dolar ");
-        System.out.println(" 3 - Dólar para Real ");
-        System.out.println(" 4 - Real para Dólar ");
-        System.out.println(" 5 - Dólar para Peso Colombiano ");
-        System.out.println(" 6 - Peso Colombiano para Dólar ");
-        System.out.println(" 7 - Sair ");
-        System.out.println(" *********************************************** ");
-
-        // Lendo a opção do usuário
-        int opcao = leia.nextInt();
-        if (opcao == 7) {
-            System.out.println("Saindo do programa...");
-            return;
-        }
-
-        System.out.print("Digite o valor que deseja converter: ");
-        double valor = leia.nextDouble();
-
-
         // Conectando URL
         String url = "https://v6.exchangerate-api.com/v6/68d58edffa9553a1549cab35/latest/USD";
 
@@ -60,37 +36,63 @@ public class Main {
         Gson gson = new Gson();
         ExchangeResponse exchange = gson.fromJson(response.body(), ExchangeResponse.class);
 
-        double resultado = 0;
+        while (true) {
+            System.out.println(" *********************************************** ");
+            System.out.println(" Seja bem vindo ao conversor de moedas! ");
+            System.out.print("\n");
+            System.out.println(" Escolha uma opção: ");
+            System.out.println(" 1 - Dólar para Peso Argentino ");
+            System.out.println(" 2 - Peso Argentino para Dolar ");
+            System.out.println(" 3 - Dólar para Real ");
+            System.out.println(" 4 - Real para Dólar ");
+            System.out.println(" 5 - Dólar para Peso Colombiano ");
+            System.out.println(" 6 - Peso Colombiano para Dólar ");
+            System.out.println(" 7 - Sair ");
+            System.out.println(" *********************************************** ");
 
-        switch (opcao) {
-            case 1: // USD → ARS
-                resultado = valor * exchange.conversion_rates.get("ARS");
-                System.out.printf("%.2f dólares equivalem a %.2f pesos argentinos.%n", valor, resultado);
-                break;
-            case 2: // ARS → USD
-                resultado = valor / exchange.conversion_rates.get("ARS");
-                System.out.printf("%.2f pesos argentinos equivalem a %.2f dólares.%n", valor, resultado);
-                break;
-            case 3: // USD → BRL
-                resultado = valor * exchange.conversion_rates.get("BRL");
-                System.out.printf("%.2f dólares equivalem a %.2f reais.%n", valor, resultado);
-                break;
-            case 4: // BRL → USD
-                resultado = valor / exchange.conversion_rates.get("BRL");
-                System.out.printf("%.2f reais equivalem a %.2f dólares.%n", valor, resultado);
-                break;
-            case 5: // USD → COP
-                resultado = valor * exchange.conversion_rates.get("COP");
-                System.out.printf("%.2f dólares equivalem a %.2f pesos colombianos.%n", valor, resultado);
-                break;
-            case 6: // COP → USD
-                resultado = valor / exchange.conversion_rates.get("COP");
-                System.out.printf("%.2f pesos colombianos equivalem a %.2f dólares.%n", valor, resultado);
-                break;
-            default:
-                System.out.println("Opção inválida!");
+            // Lendo a opção do usuário
+            int opcao = leia.nextInt();
+            if (opcao == 7) {
+                System.out.println("Saindo do programa...");
+                return;
+            }
+
+            System.out.print("Digite o valor que deseja converter: ");
+            double valor = leia.nextDouble();
+
+            double resultado = 0;
+
+            switch (opcao) {
+                case 1: // USD → ARS
+                    resultado = valor * exchange.conversion_rates.get("ARS");
+                    System.out.printf("%.2f dólares equivalem a %.2f pesos argentinos.%n", valor, resultado);
+                    break;
+                case 2: // ARS → USD
+                    resultado = valor / exchange.conversion_rates.get("ARS");
+                    System.out.printf("%.2f pesos argentinos equivalem a %.2f dólares.%n", valor, resultado);
+                    break;
+                case 3: // USD → BRL
+                    resultado = valor * exchange.conversion_rates.get("BRL");
+                    System.out.printf("%.2f dólares equivalem a %.2f reais.%n", valor, resultado);
+                    break;
+                case 4: // BRL → USD
+                    resultado = valor / exchange.conversion_rates.get("BRL");
+                    System.out.printf("%.2f reais equivalem a %.2f dólares.%n", valor, resultado);
+                    break;
+                case 5: // USD → COP
+                    resultado = valor * exchange.conversion_rates.get("COP");
+                    System.out.printf("%.2f dólares equivalem a %.2f pesos colombianos.%n", valor, resultado);
+                    break;
+                case 6: // COP → USD
+                    resultado = valor / exchange.conversion_rates.get("COP");
+                    System.out.printf("%.2f pesos colombianos equivalem a %.2f dólares.%n", valor, resultado);
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+
+            System.out.println(); // quebra de linha entre repetições
         }
-        leia.close();
 
     }
 }
